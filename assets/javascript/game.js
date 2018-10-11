46570c93b59ba6ac0c52ae6306ee1874aa6a6e42
R.E. Moose <rachelemoose@gmail.com>
2018-10-11 16:05:01 -0400

    everything eexcept the array sorted

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,42 +4,53 @@ var losses = 0;
 
 //creating a variable for guesses left, which starts at 9. 
 var guesses = 9; 
+var guessesLeft = 9; 
 
 //creating an array that lists out all of the options the computer could guess from
 var computerchoices = ["a", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "f", "s", "d", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
-
-var lettersGuessed = [];
 
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("loses");
 var guessesText = document.getElementById("Guesses_Left"); 
 var guessesDoneText = document.getElementById("guesses_so_far");
+//letters the player has typed
 var playerGuess;
-var i = 9; //need to find a way to fix this to go down
-//need to make computerguess store one variable per game 
-var computerGuess = computerchoices[Math.floor(Math.random() * computerchoices.length)];
-document.onkeyup = function(event) {
-    // computerGuess = computerchoices[Math.floor(Math.random() * computerchoices.length)];
-playerGuess = event.key;
-if (guesses >= 0) {
-    if (playerGuess === computerGuess) {
-        wins++;
-        console.log(playerGuess);
-    } else {
-        guesses--;
-        console.log(computerGuess);
-    }
-    if ((guesses === 0) && (playerGuess !== computerGuess)); {
-        losses++;
-        // alert("You've lost!") - currently this comments everytime you lose
-    };
-    guessesDoneText.textContent = "Your guesses so far: " + playerGuess;
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
+var computerGuess;
+var lettersGuessed = [];
 
-    guessesText.textContent = "Guesses Left: " + i--; 
-};
-};
+//need to make computerguess store one variable per game 
+computerGuess = computerchoices[Math.floor(Math.random() * computerchoices.length)];
+console.log(computerGuess)
+document.onkeyup = function(event) {
+    playerGuess = event.key;
+    if (playerGuess === computerGuess) {
+            wins++;
+            guesses = 9;
+            console.log(playerGuess + " was correct");
+            alert("You're a psychic! You guessed " + playerGuess + " which is exactly what I was thinking!")
+        };
+        if (playerGuess !== computerGuess) {
+            guesses--;
+            lettersGuessed = [];
+            console.log("Player guessed " + playerGuess + " which was wrong")
+        };
+        if (guesses === 0) {
+            losses++;
+            guesses = 9;
+            alert("I'm sorry, you are not a psychic. Try again!")
+        };
+        guessesText.textContent = "Guesses Left: " + guesses; 
+        guessesDoneText.innerHTML = "Your guesses so far: " + lettersGuessed.join(", ");
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+    };
+        
+
+
+
+
+    // guessesText.textContent = "Guesses Left: " + i--; 
+// };
 
 // learned this needs to be IN my function
 // guessesDoneText.textContent = "Your guesses so far: " + playerGuess; 
